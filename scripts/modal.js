2939e6f3980ee2195a510b84458b31da754f8591
“Vladyslav.Fedoruk” <mr.fedoruk17@gmail.com>
2023-05-26 22:41:22 +0300

    partly ready

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,6 +1,6 @@
 let modal = document.querySelector('#toothModal');
 let body = document.querySelector('body');
-let btn = document.querySelector("#toothBtn");
+let btn = document.querySelector(".toothBtn");
 let span = document.querySelector(".close");
 
 btn.onclick = function () {
@@ -30,4 +30,178 @@ function closeModal() {
     modal.style.display = "none";
     body.classList.remove("active"); // 
     body.style.overflow = "visible";
+}
+
+let modal2 = document.querySelector('#toothModal');
+let body2 = document.querySelector('body');
+let btn2 = document.querySelector(".toothBtn2");
+let span2 = document.querySelector(".close");
+
+btn2.onclick = function () {
+    modal2.style.display = "block";
+    body2.classList.add("active");
+}
+
+span2.onclick = function () {
+    modal2.style.display = "none";
+    body2.classList.remove("active");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal2.style.display = "none";
+        body2.classList.remove("active");
+    }
+}
+
+function openModal() {
+    if (modal2.style.display === "block") {
+        body2.classList.add("active");
+    }
+}
+
+function closeModal() {
+    modal2.style.display = "none";
+    body2.classList.remove("active"); // 
+    body2.style.overflow = "visible";
+}
+
+
+let modal3 = document.querySelector('#toothModal');
+let body3 = document.querySelector('body');
+let btn3 = document.querySelector(".toothBtn3");
+let span3 = document.querySelector(".close");
+
+btn3.onclick = function () {
+    modal3.style.display = "block";
+    body3.classList.add("active");
+}
+
+span3.onclick = function () {
+    modal3.style.display = "none";
+    body3.classList.remove("active");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal3.style.display = "none";
+        body3.classList.remove("active");
+    }
+}
+
+function openModal() {
+    if (modal3.style.display === "block") {
+        body3.classList.add("active");
+    }
+}
+
+function closeModal() {
+    modal3.style.display = "none";
+    body3.classList.remove("active"); // 
+    body3.style.overflow = "visible";
+}
+
+
+let modal4 = document.querySelector('#toothModal');
+let body4 = document.querySelector('body');
+let btn4 = document.querySelector(".toothBtn4");
+let span4 = document.querySelector(".close");
+
+btn4.onclick = function () {
+    modal4.style.display = "block";
+    body4.classList.add("active");
+}
+
+span4.onclick = function () {
+    modal4.style.display = "none";
+    body4.classList.remove("active");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal4.style.display = "none";
+        body4.classList.remove("active");
+    }
+}
+
+function openModal() {
+    if (modal4.style.display === "block") {
+        body4.classList.add("active");
+    }
+}
+
+function closeModal() {
+    modal4.style.display = "none";
+    body4.classList.remove("active"); // 
+    body4.style.overflow = "visible";
+}
+
+
+let modal5 = document.querySelector('#toothModal');
+let body5 = document.querySelector('body');
+let btn5 = document.querySelector(".toothBtn5");
+let span5 = document.querySelector(".close");
+
+btn5.onclick = function () {
+    modal5.style.display = "block";
+    body5.classList.add("active");
+}
+
+span5.onclick = function () {
+    modal5.style.display = "none";
+    body5.classList.remove("active");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal5.style.display = "none";
+        body5.classList.remove("active");
+    }
+}
+
+function openModal() {
+    if (modal5.style.display === "block") {
+        body5.classList.add("active");
+    }
+}
+
+function closeModal() {
+    modal5.style.display = "none";
+    body5.classList.remove("active"); // 
+    body5.style.overflow = "visible";
+}
+
+
+let modal6 = document.querySelector('#toothModal');
+let body6 = document.querySelector('body');
+let btn6 = document.querySelector(".toothBtn6");
+let span6 = document.querySelector(".close");
+
+btn6.onclick = function () {
+    modal6.style.display = "block";
+    body6.classList.add("active");
+}
+
+span6.onclick = function () {
+    modal6.style.display = "none";
+    body6.classList.remove("active");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal6.style.display = "none";
+        body6.classList.remove("active");
+    }
+}
+
+function openModal() {
+    if (modal6.style.display === "block") {
+        body6.classList.add("active");
+    }
+}
+
+function closeModal() {
+    modal6.style.display = "none";
+    body6.classList.remove("active"); // 
+    body6.style.overflow = "visible";
 }
